3ce969dc3e3c67d91577e335d4308a8e4e7630b4
Burry1234 <lars.burry1@gmail.com>
2025-07-11 09:37:21 +0200

    TestAPI3

---
+++ TESTAPI/API_TEST.py
@@ -0,0 +1,21 @@
+import requests
+
+# CoinGecko API-Endpunkt
+url = 'https://api.coingecko.com/api/v3/simple/price'
+
+# Parameter: Wir wollen den Preis von Bitcoin in USD
+params = {
+    'ids': 'bitcoin',
+    'vs_currencies': 'usd'
+}
+
+try:
+    response = requests.get(url, params=params)
+    response.raise_for_status()  # Fehlermeldung, wenn der Statuscode nicht 200 ist
+    data = response.json()
+    
+    price = data['bitcoin']['usd']
+    print(f"Aktueller Bitcoin-Preis: ${price}")
+
+except requests.exceptions.RequestException as e:
+    print(f"Fehler bei der API-Anfrage: {e}")
